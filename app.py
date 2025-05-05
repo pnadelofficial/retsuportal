2a5fd3a9c4b743578abcba14928ff5b879322c5e
pnadelofficial <75741790+pnadelofficial@users.noreply.github.com>
2025-05-05 19:21:46 +0000

    excel format

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ st.title("RetsuStatus")
 @st.cache_resource
 def download_google_sheet():
     url = "https://docs.google.com/spreadsheets/d/11eOF_az4j1eJoARD6T791TAV8nV7DyBvJSJlW-mNr1w/edit?usp=sharing"
-    output = "retsu.csv"
+    output = "retsu.xlsx"
     gdown.download(url=url, output=output, fuzzy=True)
 
 download_google_sheet()
-df = pd.read_csv("retsu.csv")
+df = pd.read_excel("retsu.xlsx")
 for i, row in df.iterrows():
     for col in row.columns:
         st.write(col)
