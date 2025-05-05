6756da20e64afa7da1a0bf3a882cda44ca8c9356
pnadelofficial <75741790+pnadelofficial@users.noreply.github.com>
2025-05-05 19:13:02 +0000

    start of site

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import gspread
+import pandas as pd
+
+st.title("RetsuStatus")
+
+@st.cache_resource
+def load_google_sheet():
+    CREDS = st.secrets['gsp_secrets']['my_project_settings']
+    gc = gspread.service_account_from_dict(CREDS)
+    return gc.open('Retsupurae Library').sheet1 # gmail account
+
+rl = load_google_sheet()
+df = pd.DataFrame(rl.get_all_records())
+for i, row in df.iterrows():
+    for col in row.columns:
+        st.write(col)
+        st.write(row[col])
 
